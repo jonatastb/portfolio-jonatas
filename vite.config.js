7c532cc96309cdb2e836a01e4b770cadf3758687
JonatasTB <101901086+jonatastb@users.noreply.github.com>
2025-05-21 23:18:18 -0300

    Deploy github part 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio-jonatas/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['js-big-decimal']
